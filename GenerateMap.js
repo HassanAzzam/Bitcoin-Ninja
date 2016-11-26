473f469ad3dd549e1843138bf4ffb070cba136d4
HassanAzzam <hassanazzam95@gmail.com>
2016-11-26 19:34:16 +0200

    Add Guard

--- GenerateMap.js
+++ GenerateMap.js
@@ -1,5 +1,5 @@
 function GenerateMap(){
-    var arr= new Array(10);
+    var arr=new Array(10);
     for(var i=0;i<10;i++) arr[i]=[0,1,0,1,0,1,1,1,1,0];
     return arr;
 }
--- index.js
+++ index.js
@@ -1,21 +1,69 @@
-
+var GuardMove=[];
+function ClearQueue(){
+    for(var k = GuardMove.length - 1; k >= 0; k--) {
+        GuardMove[k].classList.remove('empty-active');
+        GuardMove[k].setAttribute('onclick',"AddGuard(this)");
+        GuardMove.splice(k, 1);
+    }
+}
+function Extend(cell,add){
+    cell.classList.remove('empty');
+    ClearQueue();
+    cell.style.backgroundColor= "rgba(255,0,0,0.025)";
+    cell.style.outline="1px solid rgba(255,0,0,0.2)";
+    var i=parseInt(cell.id[0],10);
+    var j=parseInt(cell.id[1],10);
+    if(add){
+        game.innerHTML += ('<div class="cell char guard"></div>');
+        game.lastChild.style.top=(i*60)+"px";
+        game.lastChild.style.left=(j*60)+"px";
+    }
+    cell.setAttribute('onclick',"");
+    var adj=[
+        document.getElementById(""+(i-1)+j),
+        document.getElementById(""+(i+1)+j),
+        document.getElementById(""+i+(j-1)),
+        document.getElementById(""+i+(j+1)),
+    ];
+    adj.forEach(function(item){
+        if(item==null||!item.classList.contains('empty')) return;
+        item.classList.add('empty-active');
+        item.setAttribute('onclick',"AddRange(this)");
+        GuardMove.push(item);
+    });
+}
+function AddRange(cell){
+    Extend(cell,0);
+    var i=parseInt(cell.id[0],10);
+    var j=parseInt(cell.id[1],10);
+    guards[guards.length-1].push({i,j});
+    cell.style.backgroundImage= null;
+}
+function AddGuard(cell){
+    Extend(cell,1);
+    var i=parseInt(cell.id[0],10);
+    var j=parseInt(cell.id[1],10);
+    guards.push([{i,j}]);
+    console.log(guards[guards.length-1]);
+}
 function CreateMap(){
     var game=document.getElementById('game');
     for(var i=0;i<map.length;i++){
         for(var j=0;j<map[i].length;j++){
-            game.append('<div class="cell '++'"></div>');
+            game.innerHTML += ('<div id="'+i+''+j+'" class="cell '+((map[i][j])? 'empty':'wall')+'"></div>');
+            if((i==9&&j==4)||(i==2&&j==4)) {
+                game.lastChild.classList.toggle('empty');
+                continue;
+            }
+            if(map[i][j])
+            game.lastChild.setAttribute('onclick',"AddGuard(this)");
         }
     }
+    game.innerHTML += ('<div id="ninja" class="cell char"></div>');
+    game.innerHTML += ('<div id="coin" class="cell char"></div>');
+    //game.innerHTML += ('<div class="cell char guard"></div>');
 }
 var map = GenerateMap();
-/*
-    0000000000
-    0000000000
-    0000000000
-    0000000000
-    0000000000
-    0000000000
-
-
-
-*/
+var guards=[];
+map[9][4]=map[2][4]=1;
+CreateMap();
